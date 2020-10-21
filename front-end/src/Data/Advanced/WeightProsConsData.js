63d78462e6066f5afc261234962a30250895ddd1
Kachii <Kacheokcheong@gmail.com>
2020-10-21 23:55:55 +0100

    ammended the weight pros and cons

---
+++ front-end/src/Data/Advanced/WeightProsConsData.js
@@ -0,0 +1,23 @@
+const WeightProsCons = [
+    // The more weight you cut...
+    // Cutting too much weight will...
+    {
+        pro:"The more likely you are to have size advantage rather than a disadvantage over your oponnent.",
+        con:"Diminish your cardio and you are more likely to gas out during the fight."
+    },
+    {
+        pro:"The more likely you are to have a strength advantage over your oponnent in the grappling exchanges.",
+        con:"Mean there is less water in your brain and dehydration means you are far more susceptible to damage during the fight."
+
+    },
+    {
+        pro:"The more power you'll have in your punches and kicks",
+        con:"Have long term effects on the health of your kidney."
+    },
+    {
+        pro:"The more it builds discipline in the lead up to the fight.",
+        con:"It will have long term effects on the health of your kidney."
+    }
+]
+
+export default WeightProsCons
--- front-end/src/Pages/Advanced/WeightClasses/Components/WeightCutProsCons.js
+++ front-end/src/Pages/Advanced/WeightClasses/Components/WeightCutProsCons.js
@@ -2,40 +2,32 @@ import React from 'react'
 // CSS
 // Components
 import {Icon, Table } from 'semantic-ui-react'
+import WeightCutProsCons from '../../../../Data/Advanced/WeightProsConsData'
 
 const WeightCutProsAndCons = () => {
+
+    const ProsAndCons = WeightCutProsCons.map((point, index) => (
+        <Table.Row key={index}>
+            <Table.Cell className="weight-cut-pros">{point.pro}</Table.Cell>
+            <Table.Cell className="weight-cut-cons">{point.con}</Table.Cell>
+        </Table.Row>
+    ))
+            
 
   return (
       <>
         <h5>Pros and Cons of Weight Cutting</h5>
         <div className="pros-cons-container">
-
             <Table celled fixed>
-            <Table.Header>
-                <Table.Row>
-                    <Table.HeaderCell>The more weight you cut... <Icon name='checkmark' /></Table.HeaderCell>
-                    <Table.HeaderCell>Cutting too much weight will... <Icon name='crossmark' /></Table.HeaderCell>
-                </Table.Row>
-            </Table.Header>
-
-            <Table.Body>
-
-                <Table.Row>
-                    <Table.Cell className="weight-cut-pros">The more likely you are to have size advantage over your oponnent.</Table.Cell>
-                    <Table.Cell className="weight-cut-cons"> Drastic weight cuts will diminish more of your cardio during the fight.</Table.Cell>
-                </Table.Row>
-
-                <Table.Row>
-                    <Table.Cell className="weight-cut-pros">The more likely you are to have a strength advantage over your oponnent in the grappling exchanges.</Table.Cell>
-                    <Table.Cell className="weight-cut-cons">Less water in your brain from dehydration means you are far more susceptible to damage during the fight.</Table.Cell>
-                </Table.Row>
-                
-                <Table.Row>
-                    <Table.Cell className="weight-cut-pros">The more it builds discipline in the lead up to the fight.</Table.Cell>
-                    <Table.Cell className="weight-cut-cons">It will have long term effects on the health of your kidney.</Table.Cell>
-                </Table.Row>
-
-            </Table.Body>
+                <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell>The more weight you cut... <Icon name='checkmark' /></Table.HeaderCell>
+                        <Table.HeaderCell>Cutting too much weight will... <Icon name='crossmark' /></Table.HeaderCell>
+                    </Table.Row>
+                </Table.Header>
+                <Table.Body>
+                    {ProsAndCons}
+                </Table.Body>
             </Table>
         </div>
       </>
