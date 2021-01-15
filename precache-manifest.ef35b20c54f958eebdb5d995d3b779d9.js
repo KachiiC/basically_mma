7222fff658c2e8524f276727a5e1778fa4e9ce75
Kachii <Kacheokcheong@gmail.com>
2021-01-15 04:42:45 +0000

    Updates

---
+++ precache-manifest.ef35b20c54f958eebdb5d995d3b779d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4d5cd27e5b48f74ed9737493968a609",
+    "url": "/basically_mma/index.html"
+  },
+  {
+    "revision": "178d33d5d97d4a728361",
+    "url": "/basically_mma/static/css/2.fceb1b9f.chunk.css"
+  },
+  {
+    "revision": "7cfd56b0c105f3a9f694",
+    "url": "/basically_mma/static/css/main.58f37580.chunk.css"
+  },
+  {
+    "revision": "178d33d5d97d4a728361",
+    "url": "/basically_mma/static/js/2.fec24241.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/basically_mma/static/js/2.fec24241.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cfd56b0c105f3a9f694",
+    "url": "/basically_mma/static/js/main.226f517b.chunk.js"
+  },
+  {
+    "revision": "f68813dfc985683c2f58",
+    "url": "/basically_mma/static/js/runtime-main.aba3ce24.js"
+  }
+]);
