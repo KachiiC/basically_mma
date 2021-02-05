7a96ca8ae60411618fe5f5919e738ba311c3e3a4
Kachii <Kacheokcheong@gmail.com>
2021-02-05 21:01:09 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/basically_mma/precache-manifest.ac22bac8569e350311efc7c841094c95.js"
+  "/basically_mma/precache-manifest.c8e105d2480b7ceb4c93689daa4f275c.js"
 );
 
 self.addEventListener('message', (event) => {
