140598701f76f84346eddbcefcc0aae8c3916065
Kachii <Kacheokcheong@gmail.com>
2021-02-13 21:46:45 +0000

    edit best of

--- src/Pages/MMAWorld/BestOfMMA/index.js
+++ src/Pages/MMAWorld/BestOfMMA/index.js
@@ -1,9 +1,20 @@
 import React from 'react'
 // CSS
 // Components
+import BestFighters from 'Pages/MMAWorld/BestOfMMA/PageComponents/BestFighters'
+import BestFights from 'Pages/MMAWorld/BestOfMMA/PageComponents/BestFights'
+import BestKnockouts from 'Pages/MMAWorld/BestOfMMA/PageComponents/BestKnockouts'
+import BestSubmissions from 'Pages/MMAWorld/BestOfMMA/PageComponents/BestSubmissions'
 
 const BestOfMMA = () => (
-    <h1>Coming Soon</h1>
+
+    <div>
+        <BestFighters />
+        <BestFights />
+        <BestKnockouts />
+        <BestSubmissions />
+    </div>
+
 )
 
 
